e761195ec2a4479ac74d9d7c408064d355af18b5
Nikola Orlovski <nikikiko1@abv.bg>
2022-12-17 20:54:37 +0200

    New changes

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,30 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
-  });
+         });
+
+    
+         const img = document.querySelector(".image");
+    let scale = false
+    img.addEventListener("click", () => {      
+      if (scale) {
+        img.style.transform = "scale(1)"
+        img.style.transition = "transform 0.25s ease";
+        scale = false;
+        return;
+      }
+    
+      if (!scale) {
+        img.style.transform = "scale(2)";
+        img.style.transition = "transform 0.25s ease";
+        scale = true;
+        return;
+      }
+      console.log(scale);
+       
+    }); 
+ 
 });
